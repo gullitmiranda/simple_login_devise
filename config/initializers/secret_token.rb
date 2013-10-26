b020831b22d350a95450330994f177d7a1f71e09
Gullit Miranda <gullitmiranda@requestdev.com.br>
2013-10-26 04:01:21 +0000

    simple login app whith devise

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SimpleLoginDevise::Application.config.secret_key_base = '92102f16a2207468c7829f78d04724aadb4d47dece4db8031af15304da6cf35fd1929bd140944691f7030a748730db9f4fb56159acb486ab96476cfd03eaa4c9'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SimpleLoginDevise::Application.config.session_store :cookie_store, key: '_simple_login_devise_session'
